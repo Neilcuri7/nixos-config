8c2c77f3f5fab2a57f09c00bb12f0a8d4b400610
Neilcuri7 <u20231b866@upc.edu.pe>
2026-09-10 22:47:04 -0500

    aaaaaa

--- home/rylai/hyprland/keybinds.nix
+++ home/rylai/hyprland/keybinds.nix
@@ -97,11 +97,11 @@ in
       "ALT, F10, exec, gsr-ui-cli replay-save"
       "ALT, F11, exec, gsr-ui-cli replay-save-1-min"
       "ALT, F12, exec, gsr-ui-cli replay-save-10-min"
-      "$mainMod, D, exec, bash -c \"curl -s -X POST \\\"\${AOI_BACKEND_URL:-http://127.0.0.1:8000}/api/v1/audio/ptt/start\\\"\""
+      "$mainMod, D, exec, bash -c \"URL=\$(cat \${XDG_RUNTIME_DIR:-/run/user/\\$UID}/aoi_backend_url 2>/dev/null || echo \\\${AOI_BACKEND_URL:-http://127.0.0.1:8000}); curl -s -X POST \\\"\\$URL/api/v1/audio/ptt/start\\\"\""
     ];
 
     bindr = [
-      "$mainMod, D, exec, bash -c \"curl -s -X POST \\\"\${AOI_BACKEND_URL:-http://127.0.0.1:8000}/api/v1/audio/ptt/stop\\\"\""
+      "$mainMod, D, exec, bash -c \"URL=\$(cat \${XDG_RUNTIME_DIR:-/run/user/\\$UID}/aoi_backend_url 2>/dev/null || echo \\\${AOI_BACKEND_URL:-http://127.0.0.1:8000}); curl -s -X POST \\\"\\$URL/api/v1/audio/ptt/stop\\\"\""
     ];
 
     bindm = [
