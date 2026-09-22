2d510c4e8eb8cf4b7554d8722b78776e71a64501
Neilcuri7 <u20231b866@upc.edu.pe>
2026-09-22 12:16:15 -0500

    fix(croft): target vitali87/croft VSCode-style TUI editor

--- modules/shared/applications/pkgs/croft.nix
+++ modules/shared/applications/pkgs/croft.nix
@@ -1,28 +1,25 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook }:
+{ lib, rustPlatform, fetchFromGitHub, pkg-config, openssl }:
 
-stdenv.mkDerivation rec {
+rustPlatform.buildRustPackage rec {
   pname = "croft";
-  version = "0.1.1";
+  version = "0.1.942";
 
-  src = fetchurl {
-    url = "https://github.com/nsrosenqvist/croft/releases/download/v${version}/croft-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = "sha256-/LIIiK5GKS6L07catUknFALK0v8HOuYIF3d3X02LEUw=";
+  src = fetchFromGitHub {
+    owner = "vitali87";
+    repo = "croft";
+    rev = "v${version}";
+    sha256 = "sha256-CEf7Kfu4B0R7xdO3K7t0KflbqAbUaNmTFMVPMpoVODU=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
-  buildInputs = [ stdenv.cc.cc.lib ];
+  cargoHash = "sha256-H4j7W8zvMa3o2MR7RKvZz2aMUFFPN9RFI9UeDFgtHJ0=";
 
-  sourceRoot = ".";
-
-  installPhase = ''
-    install -m755 -D croft $out/bin/croft
-  '';
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl ];
 
   meta = with lib; {
-    description = "A dev-environment wrapper with embedded TUI and Docker Compose orchestration";
-    homepage = "https://github.com/nsrosenqvist/croft";
+    description = "A VSCode-style TUI text editor written in Rust";
+    homepage = "https://croft.software";
     license = licenses.mit;
-    platforms = [ "x86_64-linux" ];
     mainProgram = "croft";
   };
 }
